256b548bf0e6f702a8794084ce6a097c6a67b803
DESKTOP-MOA5RPK\EBLEE <wiseeunbi1@gmail.com>
2025-11-21 13:58:54 +0900

    [이은비] - 회사컴 빌드 오류 해결 및 코드 오류 수정

--- src/main/java/com/eunbi/springexample/ajax/AjaxController.java
+++ src/main/java/com/eunbi/springexample/ajax/AjaxController.java
@@ -14,17 +14,19 @@ import java.util.Map;
 @Controller
 public class AjaxController {
 
-
-    // response에 데이터(json)을 담는 페이지를 API라고 칭합니다.
-    // 이름과 생년월일을 전달받고, 이름과 나이를 JSON으로 응답에 담는다.
-    @GetMapping("/person")
+    // response에 데이터(JSON)를 담는 페이지
+    // API <---> View(HTML)
+    // 이름과 생년월일을 전달 받고, 이름과 나이를 JSON으로 응답에 담는다
     @ResponseBody
+    @GetMapping("/person")
     public Map<String, Object> personInfo(
             @RequestParam("name") String name
-            ,@RequestParam("birthday") String birthday) {
+            , @RequestParam("birthday") String birthday) {  // 20030523
 
-        int year = Integer.parseInt(birthday.substring(0,4));
+        int year = Integer.parseInt(birthday.substring(0, 4));
         int age = 2025 - year + 1;
+
+        // {"name":"유재석", "age":28}
 
         Map<String, Object> personMap = new HashMap<>();
 
@@ -32,12 +34,39 @@ public class AjaxController {
         personMap.put("age", age);
 
         return personMap;
-    }
 
+    }
 
 
     @GetMapping("/ex01")
     public String ex01() {
         return "ajax/ex01";
     }
+//
+//
+//    // response에 데이터(json)을 담는 페이지를 API라고 칭합니다.
+//    // 이름과 생년월일을 전달받고, 이름과 나이를 JSON으로 응답에 담는다.
+//    @GetMapping("/person")
+//    @ResponseBody
+//    public Map<String, Object> personInfo(
+//            @RequestParam("name") String name
+//            ,@RequestParam("birthday") String birthday) {
+//
+//        int year = Integer.parseInt(birthday.substring(0,4));
+//        int age = 2025 - year + 1;
+//
+//        Map<String, Object> personMap = new HashMap<>();
+//
+//        personMap.put("name", name);
+//        personMap.put("age", age);
+//
+//        return personMap;
+//    }
+//
+//
+//
+//    @GetMapping("/ex01")
+//    public String ex01() {
+//        return "ajax/ex01";
+//    }
 }
